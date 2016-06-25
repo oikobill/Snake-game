89356b0a572f24c578a5a897b31436cd023c6188
I-C-Karakozis <gianniskarakozis@gmail.com>
2016-06-25 16:18:08 +0300

    Main w/ Gamover 1

--- Game.java
+++ Game.java
@@ -1,104 +1,104 @@
 public class Game {
-	private int windowHeight;
-	private int windowWidth;
-	private int[][] board;
-	private int[] head_coordinates;
-	private int[] butt_coordinates;
-
-	public Game() {
-		this.windowWidth=60;
-		this.windowHeight=60;
-		this.board = new int[60][60];
-		head_coordinates = new int[2];
-	    butt_coordinates = new int[2];
-		this.head_coordinates[0] = windowWidth/2;
-		this.head_coordinates[1] = windowHeight/2;
-		this.butt_coordinates[0] = windowWidth/2 - 4;
-		this.butt_coordinates[1] = windowHeight/2;
-		}
-
-	public Game(int height, int width) {
-		this.windowWidth=width;
-		this.windowHeight=height;
-		this.board = new int[width][height];
-		this.head_coordinates[0] = windowWidth/2;
-		this.head_coordinates[1] = windowHeight/2;
-		this.butt_coordinates[0] = windowWidth/2 - 4;
-		this.butt_coordinates[1] = windowHeight/2;
-	}
-
-	public void drawWindow() {
-		StdDraw.clear();
+    private int windowHeight;
+    private int windowWidth;
+    private int[][] board;
+    private int[] head_coordinates;
+    private int[] butt_coordinates;
+    
+    public Game() {
+        this.windowWidth=60;
+        this.windowHeight=60;
+        this.board = new int[60][60];
+        head_coordinates = new int[2];
+        butt_coordinates = new int[2];
+        this.head_coordinates[0] = windowWidth/2;
+        this.head_coordinates[1] = windowHeight/2;
+        this.butt_coordinates[0] = windowWidth/2 - 4;
+        this.butt_coordinates[1] = windowHeight/2;
+    }
+    
+    public Game(int height, int width) {
+        this.windowWidth=width;
+        this.windowHeight=height;
+        this.board = new int[width][height];
+        this.head_coordinates[0] = windowWidth/2;
+        this.head_coordinates[1] = windowHeight/2;
+        this.butt_coordinates[0] = windowWidth/2 - 4;
+        this.butt_coordinates[1] = windowHeight/2;
+    }
+    
+    public void drawWindow() {
+        StdDraw.clear();
         StdDraw.setXscale(0.0, windowWidth);
         StdDraw.setYscale(0.0, windowHeight);       
-	}
-
-	public void generate_food(int num_foods) {
-		int x_cor;
-		int y_cor;
-		for (int i=0; i<num_foods;i++) {
-			do {
-				x_cor = (int) (Math.random()*this.windowWidth);
-				y_cor = (int) (Math.random()*this.windowHeight);
-			} while(this.board[x_cor][y_cor]!=0);
-			this.board[x_cor][y_cor]=4;
-		}
-	}
-
-	public void initializeSnake(int num_foods) {
-		// set head
-		board[head_coordinates[0]][head_coordinates[1]] = 2;
-		for (int i=1; i<=3; i++) {
-			board[windowWidth/2 - i][windowHeight/2] = 1;
-		}
-		// set butt
-		board[butt_coordinates[0]][butt_coordinates[1]] =  3;
-
-		// randomly generate food
-		generate_food(num_foods);
-	}
-
-	public void render() {
-		StdDraw.clear();
-		for(int i=0; i<this.windowWidth; i++) {
-			for (int j=0; j<this.windowHeight;j++) {
-				if (this.board[i][j]==1 |this.board[i][j]==3) {
-					StdDraw.setPenColor(StdDraw.BLACK);
-					StdDraw.filledSquare(i+0.5, j+0.5, 0.5);
-				} else if (this.board[i][j]==2) {
-					StdDraw.setPenColor(StdDraw.RED);
-					StdDraw.filledSquare(i+0.5, j+0.5, 0.5);
-				} else if (this.board[i][j]==4){
-					StdDraw.setPenColor(StdDraw.BLUE);
-					StdDraw.filledSquare(i+0.5, j+0.5, 0.5);
-				}
-			}
-		}
-	}
+    }
+    
+    public void generate_food(int num_foods) {
+        int x_cor;
+        int y_cor;
+        for (int i=0; i<num_foods;i++) {
+            do {
+                x_cor = (int) (Math.random()*this.windowWidth);
+                y_cor = (int) (Math.random()*this.windowHeight);
+            } while(this.board[x_cor][y_cor]!=0);
+            this.board[x_cor][y_cor]=4;
+        }
+    }
+    
+    public void initializeSnake(int num_foods) {
+        // set head
+        board[head_coordinates[0]][head_coordinates[1]] = 2;
+        for (int i=1; i<=3; i++) {
+            board[windowWidth/2 - i][windowHeight/2] = 1;
+        }
+        // set butt
+        board[butt_coordinates[0]][butt_coordinates[1]] =  3;
+        
+        // randomly generate food
+        generate_food(num_foods);
+    }
+    
+    public void render() {
+        StdDraw.clear();
+        for(int i=0; i<this.windowWidth; i++) {
+            for (int j=0; j<this.windowHeight;j++) {
+                if (this.board[i][j]==1 |this.board[i][j]==3) {
+                    StdDraw.setPenColor(StdDraw.BLACK);
+                    StdDraw.filledSquare(i+0.5, j+0.5, 0.5);
+                } else if (this.board[i][j]==2) {
+                    StdDraw.setPenColor(StdDraw.RED);
+                    StdDraw.filledSquare(i+0.5, j+0.5, 0.5);
+                } else if (this.board[i][j]==4){
+                    StdDraw.setPenColor(StdDraw.BLUE);
+                    StdDraw.filledSquare(i+0.5, j+0.5, 0.5);
+                }
+            }
+        }
+    }
     public void startGame() {  
         this.drawWindow();
         this.initializeSnake(2);
         while (true) {
-        	this.render();
-        	StdDraw.show(1000);
-        	updateBoard();
+            this.render();
+            StdDraw.show(1000);
+            updateBoard();
         }
     }
     
-	public void updateBoard() {
-		//head update
-		this.board[this.head_coordinates[0]+1][this.head_coordinates[1]] = 2;
-		this.board[this.head_coordinates[0]][this.head_coordinates[1]] = 1;
-		this.head_coordinates[0]++;
-
-		//head update
-		this.board[this.butt_coordinates[0]][this.head_coordinates[1]] = 0;
-		this.butt_coordinates[0]++;
-		this.render();
-	}
-
+    public void updateBoard() {
+        //head update
+        this.board[this.head_coordinates[0]+1][this.head_coordinates[1]] = 2;
+        this.board[this.head_coordinates[0]][this.head_coordinates[1]] = 1;
+        this.head_coordinates[0]++;
+        
+        //head update
+        this.board[this.butt_coordinates[0]][this.head_coordinates[1]] = 0;
+        this.butt_coordinates[0]++;
+        this.render();
+    }
+    
     public static void main(String[] args) {
-    	Game game = new Game();
-    	game.startGame();
+        Game game = new Game();
+        game.startGame();
     }
 }
--- Main.java
+++ Main.java
@@ -1,53 +1,89 @@
 public class Main {
-
-       public static void main(String[] args) {
-         while(true) {
-            // Main Menu dimensions
-            StdDraw.setXscale(0.0, 16.0);
-            StdDraw.setYscale(0.0, 16.0);
-            
-            // Main Menu Background        
-            
-            // Main Menu Play
-            StdDraw.picture(8.0, 10.0, "play.png");
-            
-            // Main Menu High Scores
-            StdDraw.picture(8.0, 6.0, "highscore.png");
-            
-            // Main Menu Exit
-            StdDraw.picture(8.0, 2.0, "exit.png");
-            
-            while (true) 
+    public static void gameOver() 
+    {        
+        // Game Over screen dimensions
+        StdDraw.setXscale(0.0, 16.0);
+        StdDraw.setYscale(0.0, 16.0);
+        
+        // Game Over Background        
+        StdDraw.picture(8.0, 8.0, "gameover.png");
+        
+        // Main Menu High Scores
+        StdDraw.picture(8.0, 8.0, "blanksquare.png");
+        StdDraw.text(8.0, 8.0, "Score: ");
+        
+        // Main Menu Retry
+        StdDraw.picture(8.0, 4.0, "retry.png");
+        
+        // Main Menu Retry
+        StdDraw.picture(8.0, 1.0, "menu.png");        
+        
+        // Button Interactions
+        
+        if (StdDraw.mousePressed()) 
+        {       
+            // if Retry clicked, initiate new Game
+            if (StdDraw.mouseX() >= 5.0 && StdDraw.mouseX() <= 11.0 && 
+                StdDraw.mouseY() >= 4.0 && StdDraw.mouseY() <= 7.0) 
             {
-                if (StdDraw.mousePressed())
-                {
-                    //  if Exit clicked, exit the program
-                    if (StdDraw.mouseX() >= 5.0 && StdDraw.mouseX() <= 11.0 &&
-                        StdDraw.mouseY() >= 1.5 && StdDraw.mouseY() <= 3.6) 
-                    {
-                        System.exit(0);
-                    }
-                    // if Play clicked
-                    else if (StdDraw.mouseX() >= 4.0 && StdDraw.mouseX() <= 12.0 &&
-                             StdDraw.mouseY() >= 9.0 && StdDraw.mouseY() <= 11.0) 
-                    {  
-                        Game game = new Game();    
-                        game.drawWindow();
-                        game.initializeSnake(2); // deal somehow with default num_foods
-                        game.render();
-                    }
-                    // if High Scores clicked
-                    else if (StdDraw.mouseX() >= 5.0 && StdDraw.mouseX() <= 11.0 &&
-                             StdDraw.mouseY() >= 5.0 && StdDraw.mouseY() <= 7.0) 
-                    {
-                        // highScores(scores);
-                         break;
-                    }
-                   
-                }
-                
+                Game game = new Game();
+                game.startGame();               
             }
-
-         }
+            // if Main Menu button clicked, return to Main Menu
+            else if (StdDraw.mouseX() >= 5.0 && StdDraw.mouseX() <= 11.0 
+                         && StdDraw.mouseY() >= 0.0 && StdDraw.mouseY() <=  3.0) 
+            {
+                MainMenu();    
+            }           
+        }
+        
+    }
+    
+    public static void MainMenu(){
+        
+        // Main Menu dimensions
+        StdDraw.setXscale(0.0, 16.0);
+        StdDraw.setYscale(0.0, 16.0);
+        
+        // Main Menu Background        
+        
+        // Main Menu Play
+        StdDraw.picture(8.0, 10.0, "play.png");
+        
+        // Main Menu High Scores
+        StdDraw.picture(8.0, 6.0, "highscore.png");
+        
+        // Main Menu Exit
+        StdDraw.picture(8.0, 2.0, "exit.png");
+        
+        while (true) 
+        {
+            if (StdDraw.mousePressed())
+            {
+                //  if Exit clicked, exit the program
+                if (StdDraw.mouseX() >= 5.0 && StdDraw.mouseX() <= 11.0 &&
+                    StdDraw.mouseY() >= 1.5 && StdDraw.mouseY() <= 3.6) 
+                {
+                    System.exit(0);
+                }
+                // if Play clicked
+                else if (StdDraw.mouseX() >= 4.0 && StdDraw.mouseX() <= 12.0 &&
+                         StdDraw.mouseY() >= 9.0 && StdDraw.mouseY() <= 11.0) 
+                {  
+                    Game game = new Game();
+                    game.startGame();
+                }
+                // if High Scores clicked
+                else if (StdDraw.mouseX() >= 5.0 && StdDraw.mouseX() <= 11.0 &&
+                         StdDraw.mouseY() >= 5.0 && StdDraw.mouseY() <= 7.0) 
+                {
+                    // highScores(scores);
+                    
+                }                
+            }          
+        }
+    }
+    public static void main(String[] args) {
+        MainMenu();
     }
 }
